8a6d123f62698c1265eab59ac122a7a3c38b5362
Bruno De Rosa <brunoderosa180@gmail.com>
2023-12-18 19:26:25 -0300

    add vue-apollo and graphql

---
+++ services/pokeapi/getMainScreenListService.js
@@ -0,0 +1,5 @@
+export default async function getMainScreenListService() {
+    const url = ''
+    
+    fetch(url)
+}
---
+++ src/components/PokeCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { toRefs } from 'vue'
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue'
+
+const props = defineProps({
+  pokemon: Object
+})
+
+const { pokemon } = toRefs(props)
+
+const { name, exp, height, weight, abilities } = pokemon
+</script>
+
+<template>
+    <ion-card>
+    <ion-card-header>
+      <ion-card-title>{{ name }}</ion-card-title>
+      <ion-card-subtitle>{{ exp }}</ion-card-subtitle>
+    </ion-card-header>
+    <ion-card-content>
+      Altura: {{ height }}
+    </ion-card-content>
+    <ion-card-content>
+      Peso: {{ weight }}
+    </ion-card-content>
+    <ion-card-content>
+      Habilidades: {{ abilities.join(', ') }}
+    </ion-card-content>
+
+</ion-card>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
